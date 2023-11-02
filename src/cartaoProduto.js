d554414c9153aad6844661e66c6ec5ec0b3a17da
gabriel <100540764+gabrieldamasio9@users.noreply.github.com>
2023-11-01 21:01:29 -0300

    feat: ecommece-carrinho

--- src/cartaoProduto.js
+++ src/cartaoProduto.js
@@ -4,11 +4,11 @@ export function renderizarCatalogo(){
     for (const produtoCatalogo of catalogo){
     
     const cartaoProduto = ` <div class=" w-48 m-2 flex flex-col p-2 shadow-xl shadow-slate-300 rounded-lg group " id="card-produto-${produtoCatalogo.id}">
-    <img src="${produtoCatalogo.Imagem}" alt="magic-clip" style="height: 200px;" class="group-hover:scale-105 duration-300 my-3 rounded-lg">
+    <img src="${produtoCatalogo.Imagem}" alt="magic-clip" style="height: 190px;" class="group-hover:scale-105 duration-500 my-3 rounded-lg">
 
     <P class="text-sm">${produtoCatalogo.marca}</P>
     <P class="text-sm">${produtoCatalogo.nome}</P>
-    <p class="text-sm">$${produtoCatalogo.preco}</p>
+    <p class="text-sm">R$${produtoCatalogo.preco},00</p>
     <button id='adiciona-${produtoCatalogo.id}' class="my-2 bg-slate-950 text-slate-200 hover:bg-slate-700"
     ><i class="fa-solid fa-cart-plus"></i></button>
     </div>`;
--- src/menucarrinho.js
+++ src/menucarrinho.js
@@ -1,5 +1,10 @@
 import { catalogo } from "./utilidades";
 
+const idsProdutoCarrinhoComQuantidade = {
+
+ 
+}
+
 function abrirCarrinho () {
 
     document.getElementById("carrinho").classList.add("right-[0]");
@@ -21,22 +26,61 @@ export function inicializarCarrinho() {
     botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
 }
 
-export function adicionarAoCarrinho (idProduto){
+
+function aumentarQuantidadeDeProduto(idProduto){
+    idsProdutoCarrinhoComQuantidade[idProduto]++;
+    atualizarInformacaoQuantidade(idProduto);
+}
+
+function diminuirQuantidadeDeProduto(idProduto){
+    idsProdutoCarrinhoComQuantidade[idProduto]--;
+    atualizarInformacaoQuantidade(idProduto);
+}
+
+function atualizarInformacaoQuantidade(idProduto){
+    document.getElementById(`quantidade-${idProduto}`).innerText = idsProdutoCarrinhoComQuantidade[idProduto];
+}
+
+
+
+export function adicionarAoCarrinho(idProduto){
+    if (idProduto in idsProdutoCarrinhoComQuantidade){
+        aumentarQuantidadeDeProduto(idProduto);
+        return;
+    }
+    idsProdutoCarrinhoComQuantidade[idProduto] = 1;
     const produto = catalogo.find((p) => p.id === idProduto);
     const conteinerProdutosCarrinho = 
     document.getElementById("produto-carrinho");
+    
+    
+    
     const cartaoProdutoCarrinho =            `<article class="flex  bg-slate-100 rounded-lg p-2 relative ">
 
     <button id="fechar-carrinho" class="absolute top-1 right-2" ><i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800" ></i></button>
     <img src="./${produto.Imagem}" alt="Maquina de corte Magic clip adicionado: ${produto.nome} " class="h-20 rounded-lg ">
     
     
-<div class="mx-1">    
-<p class="text-slate-900 text-sm" > ${produto.nome}</p>
-    <p class="text-slate-400 text-xs">Modelo: Magic Clip cordless</p>
-        <p class="text-green-500 ">$${produto.preco}</p>
+<div class="p-1 mx-1 flex flex-col justify-between">    
+    <p class="text-slate-900 text-sm" > ${produto.nome}</p>
+        <p class="text-slate-400 text-xs">Marca: ${produto.marca}</p>
+            <p class="text-green-500 ">R$${produto.preco}</p>
+</div>
+
+<div class="flex text-slate-950 items-end absolute bottom-0 right-2  text-lg">
+    <button id="decrementar-produto-${produto.id}">-</button>
+    <p id='quantidade-${produto.id}'  class="ml-2">${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
+    <button  id="incrementar-produto-${produto.id}" class="ml-2">+</button>
 </div>
 
 </article>`;
 conteinerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
+
+document
+.getElementById(`decrementar-produto-${produto.id}`)
+.addEventListener("click",() => diminuirQuantidadeDeProduto(produto.id));
+
+document
+.getElementById(`incrementar-produto-${produto.id}`)
+.addEventListener("click",() => aumentarQuantidadeDeProduto(produto.id));
 }
